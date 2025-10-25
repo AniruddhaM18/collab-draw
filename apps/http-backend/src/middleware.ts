00d029bb130fbe083e3ac7525636ded6ef7d263a
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-10-25 23:54:14 +0530

    updated middleware

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,17 +1,43 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 
-export function middleware(req: Request, res: Response, next:NextFunction) {
-    const token = req.headers["authorization"] ?? ""; 
+declare global {
+    namespace Express{
+        interface Request{
+            userId?: string;
+        }
+    }
+}
 
-    const decoded = jwt.verify(token, JWT_SECRET);
+export async function authenticateUser(req: Request, res:Response, next: NextFunction){
+    let token = req.cookies["jwt"];
+    if(!token && req.headers.authorization) {
+        const authHeader = req.headers.authorization;
+        if(authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+        }
+    }
+    if(!token) {
+        res.status(401).json({
+            message: "User not logged in"
+        });
+        return;
+    }
+    try {
+        const verified = jwt.verify(token, JWT_SECRET) as JwtPayload;
 
-    if (decoded){
-        //@ts-ignore
-        req.userId = decoded.userId;
-        next()
-    }else {
-        res.status(403).json({message: "Unauthorized"})
+        if(!verified?.id) {
+            res.status(401).json({
+                message: "User not registered, invalid token"
+            });
+            return;
+        }
+        req.userId = verified.id;
+        next();
+    }catch(err){
+        res.status(401).json({
+            messaage: "Invalid token"
+        })
     }
 }
