0ab0e30bd6b67570c725b77b36af8264c64df922
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-10-25 23:55:12 +0530

    content logic

---
+++ apps/http-backend/src/controller/content.controller.ts
@@ -0,0 +1,159 @@
+import { Request, Response } from "express";
+import { prismaClient } from "@repo/db/client";
+
+export async function fetchHomeInfo(req: Request, res: Response) {
+  const { title } = req.query;
+  const userId = req.userId;
+
+  try {
+    const rooms = await prismaClient.room.findMany({
+      where: {
+        participants: {
+          some: { id: userId },
+        },
+        ...(title ? { title: { contains: title as string } } : {}),
+      },
+      select: {
+        id: true,
+        title: true,
+        joinCode: true,
+        chat: {
+          take: 1,
+          orderBy: {
+            serialNumber: "desc",
+          },
+          select: {
+            user: {
+              select: { name: true },
+            },
+            content: true,
+          },
+        },
+      },
+    });
+
+    res.json({ rooms });
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({ message: "Could not fetch rooms" });
+  }
+}
+
+export async function fetchAllMessages(req: Request, res: Response) {
+    const userId = req.userId;
+    if(!userId) {
+        res.status(401).json({
+            message: "User Id not found"
+        })
+        return;
+    }
+    const { roomId } = req.params;
+    const { lastSrNo } = req.query;
+
+    try {
+        const userExists = await prismaClient.room.findFirst({
+            where: {
+                id: roomId,
+                participants: {
+                    some: { id : userId }
+                }
+            },
+            select: {
+                id: true,
+                title: true,
+                joinCode: true
+            }
+        });
+        if(!userExists?.id) {
+            res.status(401).json({
+                message: "Not a part of this room"
+            })
+            return;
+        }
+        let message;
+        if(lastSrNo !== undefined) {
+            message = await prismaClient.chat.findMany({
+                where: {
+                    roomId: roomId,
+                    serialNumber: {
+                        lt: parseInt(lastSrNo as string)
+                    }
+                }, 
+                select: {
+                    id: true,
+                    content :true,
+                    serialNumber: true,
+                    createdAt: true,
+                    userId: true,
+                    user: {
+                        select: {
+                            username: true
+                        }
+                    },
+                    roomId :true
+                },
+                take: 25,
+                orderBy: {
+                    serialNumber: "asc"
+                },
+            })
+        } else {
+            message = await prismaClient.chat.findMany({
+                where: {
+                    roomId : roomId
+                },
+                select: {
+                    id: true,
+                    content: true,
+                    serialNumber: true,
+                    createdAt: true,
+                    userId :true,
+                    user :{
+                        select: {
+                            username: true,
+                        }
+                    },
+                    roomId : true
+                },
+                take : 25,
+                orderBy: {
+                    serialNumber: "desc"
+                }
+            });
+        }
+        res.json({
+            message: message.reverse(),
+        });
+} catch (err) {
+    console.log(err);
+    res.status(401).json({
+        message: "Could not fetch messages"
+    })
+  }
+}
+
+export async function fetchAllDraws(req: Request, res: Response){
+    const userId = req.userId;
+    if(!userId){
+        res.status(401).json({
+            message: "User not found",
+        });
+        return
+    }
+    const { roomId } = req.params;
+    try {
+        const draws = await prismaClient.draw.findMany({
+            where: {
+                roomId :roomId
+            }
+        });
+        res.json({
+            draws
+        });
+    } catch(err) {
+        console.log(err);
+        res.status(401).json({
+            message: "Could not fetch draws"
+        });
+    }
+}
