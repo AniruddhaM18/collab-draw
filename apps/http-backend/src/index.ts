2918dad8d32753221e424363e4a1f0ca723611b6
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-10-26 15:29:11 +0530

    finished http backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,126 +1,28 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "@repo/backend-common/config";
-import { middleware } from "./middleware.js";
-import { CreateUserSchema, SigninSchema, RoomSchema } from "@repo/common/types";
-import { prismaClient } from "@repo/db/client";
+import authRoute from "./routes/authRoutes.js";
+import contentRoute from "./routes/contentRoutes.js";
+import roomRoute from "./routes/roomRoutes.js";
+import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+import { FRONTEND_URL } from "@repo/backend-common/config";
+import { SERVER_URL } from "@repo/backend-common/config";
 
 
 const app = express();
 app.use(express.json());
-app.use(cors())
-
-app.post("/signup", async (req , res) => {
-   
-    const parsedData = CreateUserSchema.safeParse(req.body);
-    if(!parsedData.success) {
-         res.json({
-            message: "Incorrect Inputs"
-        })
-        return;
-    }
-    try {
-    const user = await prismaClient.user.create({
-        data : {
-            emali: parsedData.data?.username,
-            password: parsedData.data?.password,
-            name: parsedData.data?.name
-        }
+app.use(cookieParser());
+app.use(
+    cors({
+        origin : FRONTEND_URL,
+        credentials: true
     })
-    res.json({ 
-        userId: user.id
-     })
-    }catch(err) {
-        res.status(411).json({
-            message: "User with same username already exists"
-        })
-    }
-});
+);
 
-app.post("/signin", async (req , res) => {
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/room", roomRoute);
+app.use("/api/v1/content", contentRoute);
 
-    const parsedData = SigninSchema.safeParse(req.body);
-    if(!parsedData.success) {
-         res.json({
-            message: "Incorrect Inputs"
-        })
-        return;
-    }
-
-    const user = await prismaClient.user.findFirst({
-        where: {
-            emali : parsedData.data.username,
-            password : parsedData.data.password
-        }
-    });
-    if(!user) {
-        res.status(403).json({
-            message: "Not Authorized"
-        })
-        return;
-    }
-
-    const token = jwt.sign({userId: user?.id}, JWT_SECRET);
-    res.json({ token });
-});
-
-app.post("/room", middleware, async (req, res) => {
-
-    const parsedData = RoomSchema.safeParse(req.body);
-    if(!parsedData.success) {
-         res.json({
-            message: "Incorrect Inputs"
-        })
-        return;
-    }
-
-    //@ts-ignore
-    const userId = req.userId;
-
-    try {
-        const room = await prismaClient.room.create({
-        data : {
-            slug: parsedData.data.name,
-            adminId: userId
-        }
-    })
-    res.json({ roomId: room.id })
-    }catch(err) {
-        res.status(411).json({
-            message: "Room already exists with this name"
-        })
-    }
-    
-});
-
-app.get("/chats/:roomId", async (req , res) => {
-    const roomId = Number(req.params.roomId);
-    const messages = await prismaClient.chat.findMany({
-        where: {
-            roomId : roomId
-        },
-        orderBy: {
-            id: "desc"
-        },
-        take :50
-    });
-    res.json({
-        messages
-    })
-});
-
-app.get("/room/:slug", async (req , res) => {
-    const slug = req.params.slug;
-    const room = await prismaClient.room.findFirst({
-        where: {
-            slug
-        }
-    });
-    res.json({
-        room
-    })
+app.listen(SERVER_URL, ()=> {
+    console.log("Server is running @:3001");
 })
-
-app.listen(3001, ()=> console.log("Server started @:3001"));
-
--- packages/backend-common/src/index.ts
+++ packages/backend-common/src/index.ts
@@ -1,2 +1,4 @@
 export const JWT_SECRET = process.env.JWT_SECRET || "123ANIRUddha";
 export const SALTROUNDS = process.env.SALTROUNDS || "10";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000/";
+export const SERVER_URL = process.env.SERVER_URL || "3001";
