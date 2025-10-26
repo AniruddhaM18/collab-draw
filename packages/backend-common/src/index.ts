63de1a38b950a855b6be4165c879069a781a07cb
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-10-26 22:19:00 +0530

    updated

--- packages/backend-common/src/index.ts
+++ packages/backend-common/src/index.ts
@@ -2,3 +2,4 @@ export const JWT_SECRET = process.env.JWT_SECRET || "123ANIRUddha";
 export const SALTROUNDS = process.env.SALTROUNDS || "10";
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000/";
 export const SERVER_URL = process.env.SERVER_URL || "3001";
+export const WS_PORT = process.env.WS_PORT || "8080";
