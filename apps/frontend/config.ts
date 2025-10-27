cb3bf67ef833cd7a154c434f33f786da47845650
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-10-27 15:45:41 +0530

    configs and types

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,2 +1,2 @@
-export const HTTP_BACKEND = "http://localhost:3001"
-export const WS_URL = "ws://localhost:8080";
+export const HTTP_BACKEND = process.env.HTTP_BACKEND ||"http://localhost:3001/api/v1";
+export const WS_URL = process.env.HTTP_BACKEND || "ws://localhost:8080";
