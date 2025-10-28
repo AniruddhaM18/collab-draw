6326142f9ac0532c0230bfbb4b9397c7396c14f5
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-10-28 21:17:04 +0530

    changes

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,2 +1,2 @@
-export const HTTP_BACKEND = process.env.HTTP_BACKEND ||"http://localhost:3001/api/v1";
 export const NEXT_PUBLIC_WS_URL = process.env.NEXT_PUBLIC_WS_UR || "ws://localhost:8080";
+export const NEXT_PUBLIC_HTTP_URL= process.env.NEXT_PUBLIC_HTTP_URL || "http://localhost:3001/api/v1";
